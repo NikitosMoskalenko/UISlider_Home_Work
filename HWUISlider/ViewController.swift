28b408b873b29172729c08f5b5c54b79d9093edc
Moskalenko Nikita <n.moskalneko1996@yahoo.com>
2020-02-05 06:26:05 +0300

    Initial Commit

---
+++ HWUISlider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HWUISlider
+//
+//  Created by Nikita Moskalenko on 2/5/20.
+//  Copyright © 2020 Nikita Moskalenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
